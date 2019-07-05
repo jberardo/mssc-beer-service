8407268d9b137bb5a021f5a422b9f7a2489cf367
Joao Berardo <jberardo@me.com>
2019-07-04 18:12:47 -0700

    Added validation

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -4,6 +4,12 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
+import com.sun.istack.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,18 +21,31 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDto {
 
+  @Null
   private UUID id;
+
+  @Null
   private Integer version;
 
+  @Null
   private OffsetDateTime createdDate;
+
+  @Null
   private OffsetDateTime lastModifiedDate;
 
+  @NotBlank
   private String beerName;
 
+  @NotNull
   private BeerStyleEnum beerStyle;
 
+  @Positive
+  @NotNull
   private Long upc;
 
+  @Positive
+  @NotNull
   private BigDecimal price;
 
+  private Integer quantityOnHand;
 }
