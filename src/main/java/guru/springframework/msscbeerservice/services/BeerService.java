31a11279ce8590c58d914203b9295305b5db4015
Joao Berardo <jberardo@me.com>
2019-07-09 01:27:26 -0700

    adding methods for controller

---
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservice.services;
+
+import java.util.UUID;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+
+public interface BeerService {
+    BeerDto getById(UUID beerId);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+}
--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -13,24 +13,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import guru.springframework.msscbeerservice.services.BeerService;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
 
+  private final BeerService beerService;
+  
   @GetMapping("/{beerId}")
   public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
-    return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    return new ResponseEntity<>(beerService.getById(beerId), HttpStatus.OK);
   }
 
   @PostMapping
   public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto) {
-    return new ResponseEntity(HttpStatus.CREATED);
+    return new ResponseEntity<>(beerService.saveNewBeer(beerDto), HttpStatus.CREATED);
   }
 
   @PutMapping("/{beerId}")
   public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto) {
-    return new ResponseEntity(HttpStatus.NO_CONTENT);
+    return new ResponseEntity<>(beerService.updateBeer(beerId, beerDto), HttpStatus.NO_CONTENT);
   }
 }
